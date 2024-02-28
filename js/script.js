98cd5f902134adf22b0d7e1a84705ea9f9d1b48a
Hadiur Rahman <hadiurahman139@gmail.com>
2024-02-29 04:38:45 +0600

    make some update and add a loading spinner

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const loadPhone = async () => {
+const loadPhone = async (name) => {
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${name}`
   );
   const data = await res.json();
   //    1. where to append the div ;
@@ -10,9 +10,22 @@ const loadPhone = async () => {
 
 function displayPhone(phones) {
   const phoneContainer = document.getElementById("phone-container");
-  //   console.log(phones);
-  phones.forEach((phone) => {
+  // #  clear phone container cards before adding new cards;
+  //   phoneContainer.innerHTML = "";
+  phoneContainer.textContent = "";
+  //   show all btn
+  console.log(phones.length);
+  const showAll = document.getElementById("show-all");
+  if (phones.length > 12) {
+    showAll.classList.remove("hidden");
+  } else showAll.classList.add("hidden");
+  //  display only first 12 phone;
+  let phonesSlice = phones.slice(0, 12);
+  console.log(phonesSlice.length);
+  //   for loop section
+  phonesSlice.forEach((phone) => {
     console.log(phone);
+
     //2. create a div
     const phoneCard = document.createElement("div");
     //3.create a class list for the div(can copy from daisy ui)
@@ -45,9 +58,27 @@ function displayPhone(phones) {
             </div>
             
 `;
-
+    // 5.append the child
     phoneContainer.appendChild(phoneCard);
   });
+  //   hide the spinner
+  toggleLoadingSpinner(false);
 }
 
-loadPhone();
+const handleSearch = () => {
+  // show the spinner
+  toggleLoadingSpinner(true);
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  //   console.log(searchText);
+  loadPhone(searchText);
+};
+
+const toggleLoadingSpinner = (isLoading) => {
+  const loadingSpinner = document.getElementById("loading-spinner");
+  if (isLoading) {
+    loadingSpinner.classList.remove("hidden");
+  } else {
+    loadingSpinner.classList.add("hidden");
+  }
+};
